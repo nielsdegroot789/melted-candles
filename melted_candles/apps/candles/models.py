9c62e627e0bf70c9cdeacba69fbd578afe6c4364
nielsdegroot789 <niels.degroot@coachmeapp.be>
2022-04-05 08:37:00 +0200

    fix model issue

--- melted_candles/apps/candles/models.py
+++ melted_candles/apps/candles/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from apps.base.models import BaseModel
 
 class Candle(BaseModel):
-    name: models.CharField(max_length=100)
-    description: models.TextField()
-    price: models.DecimalField()
-    picture: models.ImageField()
-    
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    picture = models.ImageField()
+
     def __str__(self):
         return self.title
