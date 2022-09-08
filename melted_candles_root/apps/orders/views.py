956c55bc743cc9026576b555257a2939246bd443
Niels De Groot <nielsdegroot@live.be>
2022-09-08 12:36:36 +0200

    rename melted_candles

--- melted_candles_root/apps/orders/views.py
+++ melted_candles_root/apps/orders/views.py
@@ -21,14 +21,14 @@ class CreateOrder(APIView):
         if order["payment_method"] == 2:
             checkout_url = createStripeCheckout(order)
             return Response(checkout_url, status=status.HTTP_201_CREATED)
-        createCheckout(order)
+        sendOrderMail(order)
         return Response(status=status.HTTP_201_CREATED)
 
         #  TODO: give images to api
         # https://stripe.com/docs/api/products/object
 
 
-def createCheckout(order):
+def sendOrderMail(order):
     order_products = Order_Product.objects.filter(order=order["id"]).values()
 
     begin_message = f"Hey Elien, \n Er is zojuist een bestelling geplaatst door {order['first_name']} {order['last_name']}: \n\n"
@@ -51,10 +51,9 @@ def createCheckout(order):
 def createStripeCheckout(order):
     order_products = Order_Product.objects.filter(order=order["id"]).values()
     stripe.api_key = settings.STRIPE_PRIVATE_KEY
-    begin_message = f"Hey Elien, \n Er is zojuist een bestelling geplaatst door {order['first_name']} {order['last_name']}: \n\n"
     stripe_products = []
-    product_message = ""
 
+    sendOrderMail(order)
     for order_product in order_products:
         product = Product.objects.get(id=order_product["product_id"])
         stripe_product = {
@@ -68,25 +67,14 @@ def createStripeCheckout(order):
             'quantity': order_product["amount"],
         }
 
-        product_message += f"{order_product['amount']}x {product} \n"
         stripe_products.append(stripe_product)
 
     try:
-        message = begin_message + product_message + \
-            f"\n Email: {order['email']} \n\n Nog een fijne dag gewenst broow \n groetjes de webshop"
         session = stripe.checkout.Session.create(
             line_items=stripe_products,
             mode='payment',
             success_url='http://localhost:4242/success.html',
             cancel_url='http://localhost:4242/cancel.html',
-        )
-
-        send_mail(
-            subject='Aankoop kaarsen',
-            message=message,
-            from_email='noreply@gmail.com',
-            recipient_list=['nielsdegroot@live.be', ],
-            fail_silently=False,
         )
 
     except Exception as e:
